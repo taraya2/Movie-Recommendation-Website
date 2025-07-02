64211a1b58c696a6e472a72ab88aaf876cad86ea
Thomas <taraya2@illinois.edu>
2025-07-01 23:16:03 -0500

    MovieController

--- src/main/java/com/movie/recommender/movie_recommender/controller/MovieController.java
+++ src/main/java/com/movie/recommender/movie_recommender/controller/MovieController.java
@@ -1,4 +1,45 @@
 package com.movie.recommender.movie_recommender.controller;
 
+import com.movie.recommender.movie_recommender.entity.Actor;
+import com.movie.recommender.movie_recommender.entity.Movie;
+import com.movie.recommender.movie_recommender.repository.MovieRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/movies")
 public class MovieController {
+    private final MovieRepository movieRepo;
+
+    public MovieController(MovieRepository movieRepo) {
+        this.movieRepo = movieRepo;
+    }
+
+    // Get /api/movies/{tconst}
+    @GetMapping("/{tconst}")
+    public Optional<Movie> getMovieById(@PathVariable String tconst) {
+        return movieRepo.findById(tconst);
+    }
+
+    // Get /movies/search?title=Batman
+    @GetMapping("/search")
+    public List<Movie> getMovieByTitle(@RequestParam String title) {
+        return movieRepo.findByTitleContainingIgnoreCase(title);
+    }
+
+    // GET /api/movies/top-rated?year=2010
+    @GetMapping("/top-rated")
+    public List<Movie> topRatedByYear(@RequestParam String year) {
+        return movieRepo.findTop10ByYearOrderByRatingDesc(year);
+    }
+    // GET /api/movies/recommendations?genre=Action
+    @GetMapping("/recommendations")
+    public List<Movie> recommendByGenre(@RequestParam String genre) {
+        return movieRepo.findByGenresIgnoreCaseOrderByRatingDesc(genre);
+    }
+
+
+
 }
--- src/main/java/com/movie/recommender/movie_recommender/repository/MovieRepository.java
+++ src/main/java/com/movie/recommender/movie_recommender/repository/MovieRepository.java
@@ -4,8 +4,15 @@ import com.movie.recommender.movie_recommender.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, String> {
+
+    List<Movie> findByTitleContainingIgnoreCase(String title);
+    List<Movie> findTop10ByYearOrderByRatingDesc(String year);
+    List<Movie>findByGenresIgnoreCaseOrderByRatingDesc(String genre);
+
 
 
 }
